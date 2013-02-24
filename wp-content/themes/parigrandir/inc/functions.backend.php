79a164315a84a7c8a0598535392d901be92456ea
Davy Peter Braun <davypeterbraun@gmail.com>
2013-02-24 23:16:10 +0100

    Branching out admin page customization.

--- wp-content/themes/parigrandir/inc/functions.backend.php
+++ wp-content/themes/parigrandir/inc/functions.backend.php
@@ -1,3 +1,8 @@
 <?php
 // Insert all backend functions for custom pages.
-require_once('backend/functions.page-accueil.php');
+$post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+$template_file = get_post_meta($post_id,'_wp_page_template',TRUE);
+
+if ($template_file == 'page-accueil.php') {
+	require_once('backend/functions.page-accueil.php');
+}
